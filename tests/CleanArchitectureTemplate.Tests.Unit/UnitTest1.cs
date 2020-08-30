863da0295b111b6e3e49a460d173ea004cc6b876
KTutak <ktutak1337@gmail.com>
2020-08-30 23:20:02 +0200

    Add XUnit support (Code) - Updated CleanArchitectureTemplate.Tests.EndToEnd project, - CleanArchitectureTemplate.Tests.Integration project, - CleanArchitectureTemplate.Tests.Unit project.

--- tests/CleanArchitectureTemplate.Tests.Unit/UnitTest1.cs
+++ tests/CleanArchitectureTemplate.Tests.Unit/UnitTest1.cs
@@ -1,18 +1,32 @@
+#if (xunit)
+using Xunit;
+#else
 using NUnit.Framework;
+#endif
 
 namespace CleanArchitectureTemplate.Tests.Unit
 {
     public class Tests
     {
+        #if (!xunit)
         [SetUp]
         public void Setup()
         {
         }
-
+        #endif
+        
+        #if (xunit)
+        [Fact]
+        #else
         [Test]
+        #endif
         public void Test1()
         {
+            #if (xunit)
+            Assert.True(true);
+            #else
             Assert.Pass();
+            #endif
         }
     }
 }
